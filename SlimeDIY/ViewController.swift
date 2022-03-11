6f4d23f0ca0c21532f69a5f62602d3476188f7a0
gmb728 <gmb728@gmail.com>
2022-03-11 08:22:47 +0800

    Initial Commit

---
+++ SlimeDIY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlimeDIY
+//
+//  Created by SophiaChang on 2022/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
